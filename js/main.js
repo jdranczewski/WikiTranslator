df2b5b4b3b1d868772b097deffb7780603b818a8
Jakub Dranczewski <domdlapsa@gmail.com>
2019-07-15 00:19:04 +0200

    Comments, changes in redirect handling, callback skeleton, comments

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ class Article {
         this.id = undefined;
         this.title = undefined;
         this.redirect = false;
+        this.redirectFrom = undefined;
         this.disambiguation = false;
         this.text = undefined;
+        this.onPropsReady = undefined;
+        this.onTextReady = undefined;
     }
 
+    // Used to change the article data stored in the object
     set_title(title) {
         var xhr = new XMLHttpRequest();
         var url = "https://";
@@ -23,6 +27,7 @@ class Article {
         url += "ppprop=disambiguation&";
         url += "titles=" + title;
         xhr.open("GET", url, true);
+        // 'this' changes meaning inside a function()
         var this_class = this;
         xhr.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
@@ -34,16 +39,24 @@ class Article {
     }
 
     parse_data(data) {
+        // Debug
         data = JSON.parse(data);
         debug = data;
         console.log(data);
 
+        // Set article details and fire callback once done
         this.id = Object.keys(data["query"]["pages"])[0];
-        var page = data["query"]["pages"][this.id]
+        var page = data["query"]["pages"][this.id];
         this.title = page["title"];
         this.redirect = data['query']['redirects'] !== undefined;
+        if (this.redirect) {
+            this.redirectFrom = data['query']['redirects'][0]["from"];
+        } else {
+            this.redirectFrom = undefined;
+        }
         this.disambiguation = page["pageprops"] !== undefined &&
                               page["pageprops"]["disambiguation"] !== undefined;
         this.text = undefined;
+        if (this.onPropsReady !== undefined) this.onPropsReady();
     }
 }
